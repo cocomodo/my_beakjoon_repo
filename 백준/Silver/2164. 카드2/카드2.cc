547be00642e7830a70e3ae36c966a6f315199bf0
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-16 09:48:25 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	queue<int> Q;
+	for (int i = 1; i <= n; i++) Q.push(i);
+
+	while (Q.size() != 1) {
+		Q.pop();
+		Q.push(Q.front());
+		Q.pop();
+	}
+	
+	cout << Q.front();
+}

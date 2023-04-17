d851b2f76c4724faf851febbce2933ac411cb1dc
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-17 15:00:23 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,29 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std;
 typedef long long ll;
-
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    string s;
-    ll ans = 0;
-    int cnt = 0; // 막대의 개수를 저장할 변수
-
-    cin >> s;
-    int sz = s.length();
-    for (int i = 0; i < sz; i++) {
-        if (s[i] == '(')
-            cnt++; // 막대의 시작일 경우 cnt 증가
-        else { // s[i] == ')'인 경우
-            cnt--; // 막대의 끝일 경우 cnt 감소
-            if (s[i - 1] == '(')
-                ans += cnt; // 레이저일 경우 막대의 개수만큼 ans에 추가
-            else
-                ans++; // 막대의 끝일 경우 막대 1개가 절단된 것과 동일한 상황이므로 ans에 1 추가
-        }
+using namespace std;
+string s;
+ll ans = 0;
+stack<char> st;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> s;
+  int sz = s.length();
+  for (int i = 0; i < sz; i++) {
+    if (s[i]=='(')
+      st.push(s[i]);
+    else {
+      if (s[i-1] == '(') { // 레이저일 경우
+        st.pop(); // 앞에서 막대라고 착각하고 stack에 s[i]를 넣었으므로 pop
+        ans+=st.size(); // 막대의 개수만큼 ans에 추가
+      }
+      else { // 막대의 끝일 경우
+        st.pop();  // 막대의 개수를 1 감소
+        ans++; // 막대 1개가 절단된 것과 동일한 상황이므로 ans에 1 추가
+      }
     }
-
-    cout << ans << "\n";
-    return 0;
+  }
+  cout << ans << "\n";
+  return 0;
 }

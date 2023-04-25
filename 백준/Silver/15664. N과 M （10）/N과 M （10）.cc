c73c6afde4d85a9e54a2c591f17e7e1a64857971
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-25 20:10:13 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -4,30 +4,23 @@ using namespace std;
 int n, m;
 int arr[10];
 int num[10];
-bool isused[10];
 
-void func(int k) {
+void func(int k, int st) {
 	if (k == m) {
 		for (int i = 0; i < m; i++)
-			cout <<num[arr[i]] << ' ';
+			cout << arr[i] << ' ';
 		cout << '\n';
 		return;
 	}
-	int st = 0;
 	int tmp = 0;
-	if (k != 0) st = arr[k - 1]+1;
-	for (int i = st; i < n; i++) {
-		if (isused[i]) continue;
-		if (tmp == num[i]) continue;
-		isused[i] = 1;
-		arr[k] = i;
-		tmp = num[i];
-		func(k + 1);
-		isused[i] = 0;
+	for (int i = st; i < n; ++i) {
+		if (tmp != num[i]) {
+			arr[k] = num[i];
+			tmp = arr[k];
+			func(k + 1, i + 1);
+		}
 	}
-	return;
 }
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -35,6 +28,5 @@ int main() {
 	for (int i = 0; i < n; i++)
 		cin >> num[i];
 	sort(num, num + n);
-	func(0);
-	return 0;
+	func(0, 0);
 }

21a05022713cb0c30015b48b3df9f3054c4a83c3
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-26 11:26:22 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -2,57 +2,42 @@
 using namespace std;
 
 int L, C;
-char pas[20];
-char words[20];
-bool isused[20];
+int arr[20];
+char s[20];
 
 bool aeiou(char t) {
 	return t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u';
 }
 
-void func(int k,int st) {
 
+void func(int k, int st) {
 	if (k == L) {
 		bool flag = false;
 		int cnt1 = 0;
 		int cnt2 = 0;
 		for (int i = 0; i < L; i++) {
-			if (aeiou(pas[i])) cnt1++;
+			if (aeiou(s[arr[i]])) cnt1++;
 			else cnt2++;
 		}
-		if (cnt1 >= 1 && cnt2 >= 2) flag = true;
+		if (cnt1 >=1 && cnt2 >= 2) flag = true;
 
 		if (flag) {
 			for (int i = 0; i < L; i++)
-				cout << pas[i];
+				cout << s[arr[i]];
 			cout << '\n';
 		}
 	}
-
-	/*if (k == L) {
-		for (int i = 0; i < L; i++)
-			cout << pas[i];
-		cout << '\n';
-		return;
-	}
-	}*/
 	for (int i = st; i < C; i++) {
-		//if (isused[i]) continue;
-		//isused[i] = 1;
-		pas[k] = words[i];
+		arr[k] = i;
 		func(k + 1, i + 1);
-		//isused[i] = 0;
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	cin >> L >> C;
-	for (int i = 0; i < C; i++)
-		cin >> words[i];
-	sort(words, words + C);
-	func(0,0);
-	return 0;
+	for (int i = 0; i < C; i++) cin >> s[i];
+	sort(s, s + C);
+	func(0, 0);
 }

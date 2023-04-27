1f128312a358f6f2c8be28c3e30ba23e2cb9ddd0
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-27 17:26:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,50 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
+int n;
+string board[30];
+bool vis[30][30];
+int cnt;
+
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
-int n;
-string board[27];
-int vis[27][27];
+
+vector<int> ans;
 
 int main() {
-	ios::sync_with_stdio(0); 
-	cin.tie(0); 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++)
 		cin >> board[i];
-	int cnt = 0; 
-	vector<int> ans;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (board[i][j] == '0' || vis[i][j] == 1)
-				continue;
-			queue<pair<int, int>> Q;
-			vis[i][j] = 1;
-			Q.push({ i,j });
-			int area = 1;
+			if (board[i][j] == '0' || vis[i][j] == true) continue;
 			cnt++;
-			while (!Q.empty()) {
-				auto cur = Q.front(); Q.pop(); 
+			int area = 1;
+			queue<pair<int, int>> q;
+			vis[i][j] = true;
+			q.push({ i,j });
+			while (!q.empty()) {
+				int x, y;
+				tie(x, y) = q.front(); q.pop();
 				for (int k = 0; k < 4; k++) {
-					int nx = cur.X + dx[k];
-					int ny = cur.Y + dy[k];
+					int nx = x + dx[k];
+					int ny = y + dy[k];
 					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-					if (board[nx][ny] == '0' || vis[nx][ny] == 1) continue;
-					vis[nx][ny] = 1;
-					Q.push({ nx,ny });
+					if (board[nx][ny] == '0' || vis[nx][ny] == true) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = true;
 					area++;
 				}
 			}
 			ans.push_back(area);
 		}
 	}
-	cout << cnt << "\n";
+	cout << cnt<<'\n';
 	sort(ans.begin(), ans.end());
-	for (int i : ans)
-		cout << i << "\n";
-	return 0;
+	for (auto c : ans) {
+		cout << c << '\n';
+	}
 }

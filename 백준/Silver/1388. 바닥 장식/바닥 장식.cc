912bcd2f9b3896c32d88abe7afb6ea7cd9fd41d7
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-27 20:27:14 +0900

    [Silver IV] Title: 바닥 장식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.cc
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.cc
@@ -4,58 +4,31 @@ using namespace std;
 int n, m;
 string board[60];
 bool vis[60][60];
+int ans;
 
-int dx[2] = { 1,-1 };
-int dy[2] = { 1,-1 };
-int cnt;
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	cin >> n >> m;
 	for (int i = 0; i < n; i++)
 		cin >> board[i];
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (vis[i][j] == true) continue;
-			if (board[i][j] == '-') {
-				queue<pair<int, int>> q;
-				q.push({ i,j });
-				vis[i][j] = true;
-				cnt++;
-				while (!q.empty()) {
-					int x, y;
-					tie(x, y) = q.front(); q.pop();
-					for (int k = 0; k < 2; k++) {
-						int nx = x;
-						int ny = y + dy[k];
-						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-						if (board[nx][ny] == '|' || vis[nx][ny] == true) continue;
-						q.push({ nx,ny });
-						vis[nx][ny] = true;
-					}
+			if (vis[i][j]) continue;
+			ans++;
+			vis[i][j] = true;
+			char shape = board[i][j];
+			if (shape == '-') {
+				for (int k = j + 1; k < m; k++) {
+					if (board[i][k] == '|') break;
+					vis[i][k] = true;
 				}
 			}
-			else {//board[i][j]=='|'일때
-				queue<pair<int, int>> q;
-				q.push({ i,j });
-				vis[i][j] = true;
-				cnt++;
-				while (!q.empty()) {
-					int x, y;
-					tie(x, y) = q.front(); q.pop();
-					for (int k = 0; k < 2; k++) {
-						int nx = x+dx[k];
-						int ny = y ;
-						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-						if (board[nx][ny] == '-' || vis[nx][ny] == true) continue;
-						q.push({ nx,ny });
-						vis[nx][ny] = true;
-					}
+			else {
+				for (int k = i + 1; k < n; k++) {
+					if (board[k][j] == '-') break;
+					vis[k][j] = true;
 				}
 			}
 		}
 	}
-	cout << cnt;
+	cout << ans;
 }

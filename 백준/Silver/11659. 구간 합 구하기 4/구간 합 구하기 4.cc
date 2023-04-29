6aa7f6129ba0012207865d7a3a8519bfd839a84f
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-29 20:23:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -7,19 +7,18 @@ int d[100'005];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	int n, m;
 	cin >> n >> m;
+
 	for (int i = 1; i <= n; i++)
 		cin >> a[i];
-
 	d[1] = a[1];
 	for (int i = 2; i <= n; i++)
 		d[i] = d[i - 1] + a[i];
-
-	while (m--) {
+	while(m--) {
 		int a, b;
 		cin >> a >> b;
-		cout << d[b] - d[a-1] << '\n';
+		cout << d[b] - d[a - 1] << '\n';
 	}
 }

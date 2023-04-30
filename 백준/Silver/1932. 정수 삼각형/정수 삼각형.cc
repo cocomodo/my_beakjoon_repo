17ea5fe62064c0ad45d90e7e65863bfda1fd4198
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-04-30 13:58:43 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -2,20 +2,19 @@
 using namespace std;
 
 int n;
-int a[505][505];
 int d[505][505];
+int p[505][505];
 
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0); 
 
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= i; j++)
-			cin >> a[i][j];
-	d[1][1] = a[1][1];
-	for (int i = 2; i <= n; i++)
+			cin >> d[i][j];
+	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= i; j++)
-			d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + a[i][j];
-	cout << *max_element(d[n] + 1, d[n] + n + 1);
+			p[i][j] = max(p[i - 1][j - 1], p[i - 1][j]) + d[i][j];
+	cout << *max_element(p[n] + 1, p[n] + 1 + n);
 }

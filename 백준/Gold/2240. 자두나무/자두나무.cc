be0c874615e48fa88deacbb43bbde5a590afe10b
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-02 20:43:13 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -10,11 +10,9 @@ int main() {
 
 	int t, w;
 	cin >> t >> w;
-	for (int i = 1; i <= t; i++)
+	for (int i = 1; i <= t; i++) {
 		cin >> a[i];
-
-	//자두가 안움직이고 시간이 지날때의 d[i] 값들을 초기화
-	
+	}
 	int cnt = 0;
 	for (int i = 1; i <= t; i++) {
 		if (a[i] == 1) cnt++;
@@ -22,7 +20,6 @@ int main() {
 	}
 	for (int i = 1; i <= t; i++)
 		for (int j = 1; j <= w; j++)
-			d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + (j % 2 + 1 == a[i]);
-	
-	cout << *max_element(d[t], d[t]+ w + 1);
+			d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + ((1 + j % 2) == a[i]);
+	cout << *max_element(d[t], d[t] + 1 + w);
 }

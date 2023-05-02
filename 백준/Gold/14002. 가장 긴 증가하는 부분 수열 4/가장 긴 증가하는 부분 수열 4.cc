ef055d0f0d08706e07ed1dec8ebbc8ec7409a6ff
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-03 00:57:37 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
 int a[1005];
 int d[1005];
 int pre[1005];
@@ -8,13 +9,12 @@ int pre[1005];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	int n;
 	cin >> n;
+
 	for (int i = 1; i <= n; i++)
 		cin >> a[i];
-	fill(d + 1, d + 1 + n, 1);//d[0]을 아래부분에서 접근하는지 고려해보자.
-	
+	fill(d + 1, d + 1 + n, 1);
+
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
 			if (a[j] < a[i] && d[i] < d[j] + 1) {
@@ -25,19 +25,21 @@ int main() {
 	}
 	int maxI = 1;
 	int maxD = d[1];
-	for (int i = 2; i <= n; i++) {
+	for (int i = 1; i <= n; i++) {
 		if (maxD < d[i]) {
 			maxI = i;
 			maxD = d[i];
 		}
 	}
-	deque<int> ans;
+
+	vector<int> ans;
 	int cur = maxI;
 	while (cur) {
-		ans.push_front(a[cur]);
+		ans.push_back(a[cur]);
 		cur = pre[cur];
 	}
 	cout << ans.size() << '\n';
+	reverse(ans.begin(), ans.end());
 	for (auto c : ans)
 		cout << c << ' ';
 }

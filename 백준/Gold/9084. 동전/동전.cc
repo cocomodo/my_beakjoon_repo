b8e6abbe67f2c5c0a36535621edb39af78f984c1
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-11 21:55:02 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.cc
+++ 백준/Gold/9084. 동전/동전.cc
@@ -1,40 +1,31 @@
-// 3
-// 2
-// 1 2
-// 1000
-// 3
-// 1 5 10
-// 100
-// 2
-// 5 7
-// 22
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k;
-int a[10'005];
-int d[10'005];
 int t;
+int a[25];
+int d[10'005];
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+			cin >> a[i];
+		
+		int m;
+		cin >> m; //만들어야 할 금액, 동전 1 문제에서 k에 해당
+		d[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = a[i]; j <= m; j++) {
+				d[j] += d[j - a[i]];
+			}
+		}
+		cout << d[m] << '\n';
 
-    cin>>t;
-    while(t--){    
-        cin>>n;
-        for(int i=1;i<=n; i++)
-            cin>>a[i];
-        cin>>k;
-        d[0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=a[i];j<=k;j++){
-                d[j]+=d[j-a[i]];
-            }
-        }
-        cout<<d[k]<<'\n';
-        for(int i=1;i<=k;i++)
-            d[i]=0;
-    }
+		fill(d + 1, d + 1 + m, 0);
+	}
+
 }

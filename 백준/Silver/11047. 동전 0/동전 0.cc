117d72249861bddd52a71f0171df05842b05599e
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-14 23:39:29 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -4,33 +4,15 @@ using namespace std;
 int n, k;
 int a[15];
 
-
-
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0); 
-    
+    cin.tie(0);
+    int ans=0; 
     cin>>n>>k;
-    for(int i=1; i<=n; i++)
-        cin>>a[i];
-    
-    int ans=0;
-    int q=0;
-    int r=0;
-    for(int i=n; i>0;i--){
-        if(a[i]>k) continue;
-        if(k%a[i]==0) {
-            ans+=k/a[i];
-            cout<<ans;
-            break;
-        }
-        q=k/a[i];
-        r=k%a[i];
-        
-        ans+=q;
-        // k-=q*a[i];
-        k=r;
-
+    for(int i=0; i<n; i++) cin>>a[i];
+    for(int i=n-1; i>=0; i--){
+        ans+=k/a[i];
+        k%=a[i];
     }
-    return 0;
+    cout<<ans;
 }

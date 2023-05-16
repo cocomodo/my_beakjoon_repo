669b60ab1143ec5f2f56ed7ab656a58eda286adc
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-16 20:22:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 52 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,20 +1,22 @@
-// Authored by : hgmhc
-// Co-authored by : BaaaaaaaaaaarkingDog
-// http://boj.kr/9e42012f919544dfa94544e08b2ee1fe
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n; cin >> n;
-  pair<int,string> x[n];
-  for (auto& s : x) cin >> s.first >> s.second;
-  stable_sort(x, x+n, [&](pair<int,string> a, pair<int,string> b) {
-    return a.first < b.first;
-  });
-  for (auto p : x) cout << p.first << ' ' << p.second << '\n';
+bool compare(const pair<int, string> &a,const pair<int, string> &b) {
+	return a.first < b.first;
 }
-/*
-13 to 15번째 줄의 코드가 낯설면 람다 함수 키워드로 구글링 추천
-*/
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	pair<int, string> p;
+	vector<pair<int, string>> v;
+	for (int i = 0; i < n; i++) {
+		cin >> p.first >> p.second;
+		v.push_back(p);
+	}
+	stable_sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < n; i++)
+		cout << v[i].first << ' ' << v[i].second << '\n';
+}

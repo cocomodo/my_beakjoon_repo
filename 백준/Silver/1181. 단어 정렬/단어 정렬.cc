4eb0d4b8477776474927cf6b15a1ae896cddbf62
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-20 19:52:20 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 string st[20'005];
-bool cmp(const string& a, const string& b){
-    int lena=a.size(),lenb=b.size();
-    if(lena!=lenb) return lena<lenb;
-    return a<b;
-}
+
 int main(){
     ios::sync_with_stdio(0); 
     cin.tie(0);
     int n;
     cin>>n;
     for(int i=0; i<n; i++) cin>>st[i];
-    sort(st,st+n,cmp);
+    sort(st,st+n,[](const string& a,const string& b){
+        int lena=a.size(),lenb=b.size();
+        if(lena!=lenb) return lena<lenb;
+        return a<b;
+    });
     for(int i=0; i<n; i++){
         if(i==0||st[i-1]!=st[i]) cout<<st[i]<<'\n';
     }

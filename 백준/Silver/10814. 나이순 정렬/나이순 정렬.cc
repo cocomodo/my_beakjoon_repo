c458df17d11428328314f908e9a9e619ab5d1552
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-21 14:57:29 +0900

    [Silver V] Title: 나이순 정렬, Time: 44 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(const pair<int, string> &a,const pair<int, string> &b) {
-	return a.first < b.first;
-}
-
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+	cin.tie(0); 
 	int n;
 	cin >> n;
-	pair<int, string> p;
 	vector<pair<int, string>> v;
 	for (int i = 0; i < n; i++) {
-		cin >> p.first >> p.second;
-		v.push_back(p);
+		int a = 0;
+		string st;
+		cin >> a >> st;
+		v.push_back({ a,st });
 	}
-	stable_sort(v.begin(), v.end(), compare);
-	for (int i = 0; i < n; i++)
-		cout << v[i].first << ' ' << v[i].second << '\n';
+	stable_sort(v.begin(), v.end(), [](const pair<int, string>& a, const pair<int, string>& b) {
+		return a.first < b.first;
+		});
+	for (auto c : v) {
+		cout << c.first << ' ' << c.second << '\n';
+	}
 }

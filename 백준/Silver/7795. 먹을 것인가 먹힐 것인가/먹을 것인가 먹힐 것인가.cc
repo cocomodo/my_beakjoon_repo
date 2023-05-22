9c00d76109556607f91d6d78a7f8d67acb7572ed
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-22 20:49:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 32 ms, Memory: 2440 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arrA[20'005];
-int arrB[20'005];
-
 int main() {
 	ios::sync_with_stdio(0); 
 	cin.tie(0); 
@@ -12,21 +9,26 @@ int main() {
 	while (t--) {
 		int n, m;
 		cin >> n >> m;
-		int cnt = 0;
-		for (int i = 0; i < n; i++)
-			cin >> arrA[i];
-		for (int i = 0; i < m; i++)
-			cin >> arrB[i];
-		sort(arrA, arrA + n);
-		sort(arrB, arrB + m,greater<int>()); //greater로 하면 내림차순 정렬
+		vector<pair<int, int>> v(n + m);
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (arrA[i] > arrB[j]) {
-					cnt += m - j;
-					break;
-				}
-			}
-		}//400'000'000(모두 계산해버리면 이정도)
-		cout << cnt << '\n';
+			int a;
+			cin >> a;
+			v[i] = { a,0 };
+		}
+		for (int i = n; i < n + m; i++) {
+			int b;
+			cin >> b;
+			v[i] = { b,1 };
+		}
+		sort(v.begin(), v.end());
+		int cnt = 0; //현재까지 나온 b의 갯수
+		int ans = 0; 
+		for (int i = 0; i < n + m; i++) {
+			if (v[i].second == 0)//현재 보는 수가 A에 속한 수
+				ans += cnt;
+			else //현재 보는 수가 B에 속한 수 
+				cnt++;
+		}
+		cout << ans << '\n';
 	}
 }

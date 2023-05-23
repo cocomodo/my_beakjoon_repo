7593b3a899111aa27d9e7e0cba5cef2529e89a46
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-24 02:17:46 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int,int>> v;
+int n;
+pair<int,int> s[100'005];
 
 int main(){
     ios::sync_with_stdio(0); 
     cin.tie(0); 
-
-    int n;
     cin>>n;
+    for(int i=0; i<n; i++)
+        cin>>s[i].second>>s[i].first;
+    sort(s,s+n); //먼저 끝나느 시간을 비교하고, 끝나는 시간이 동일하면 시작 시간 순으로 정렬
+    int ans=0;
+    int t=0; //현재 시간
     for(int i=0; i<n; i++){
-        int a, b;
-        cin>>a>>b;
-        v.push_back({b,a});
+        if(t>s[i].second) continue; //시작 시간이 현재 시간보다 이전인 회의라면 무시
+        ans++;//시작 시간이 현재 시간 이후인 회의를 찾았으므로 회의의 수에 1 증가
+        t=s[i].first; //현재 시간을 s[i]의 끝나는 시간으로 변경한다. 
     }
-    sort(v.begin(),v.end());
-    int cnt=1;
-    int now=v[0].first;
-    for(int i=1; i<n; i++){
-        if(now<=v[i].second) {
-            now=v[i].first;
-            cnt++;
-        }
-    }
-    cout<<cnt;
+    cout<<ans;
 }

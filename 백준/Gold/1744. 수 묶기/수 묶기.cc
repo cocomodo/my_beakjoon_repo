d4dfb5d021e2edb2e7a6d61440d41f2083a5dfae
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-26 17:03:49 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long ans;
+
+void seqSum(vector<int> v){
+    while(1<v.size()){
+        ans+=*(v.end()-1)*(*(v.end()-2));
+        v.pop_back();
+        v.pop_back();
+    }
+    if(v.size()==1) ans+=v[0];
+}
+
+int main(){
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    vector<int> seqP;
+    vector<int> seqN;
+    while(n--){
+        int t;
+        cin>>t;
+        if(t==1) ans+=1;
+        else if(t>1)
+            seqP.push_back(t);
+        else
+            seqN.push_back(t); 
+    }
+    sort(seqP.begin(),seqP.end());
+    sort(seqN.begin(),seqN.end(),greater<>());
+    seqSum(seqP);
+    seqSum(seqN);
+    cout<<ans;
+}

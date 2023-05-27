e88ae0b380c4a039a0d5aebde103124e1b9a5a8e
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-27 18:13:32 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 28 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-long long a[1005];
+typedef long long ll;
+ll a[1005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>n>>m;    
-    for(int i=0; i<n; i++)
-        cin>>a[i];
+    int n, m;
+    cin>>n>>m;
+    for(int i=0; i<n; i++) cin>>a[i];
     while(m--){
-        sort(a,a+n);
-        long long mnum=a[0]+a[1];
-        a[0]=mnum;
-        a[1]=mnum;
+        swap(a[0],*min_element(a,a+n));
+        swap(a[1],*min_element(a+1,a+n));
+        ll tmp=a[0]+a[1];
+        a[0]=tmp;
+        a[1]=tmp;
     }
-    long long sum=0;
-    for(int i=0; i<n; i++)
-        sum+=a[i];
-    cout<<sum;
+    ll ans=0;
+    for(int i=0; i<n; i++) ans+=a[i];
+    cout<<ans;
 }

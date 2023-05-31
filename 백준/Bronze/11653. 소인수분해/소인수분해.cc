6f6416cddd885b115d53ac1feb220d7f69571f37
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-05-31 17:12:48 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    int n;
     cin>>n;
-    int a=2;
-    while(true){
-        if(n==1) break;
-        if(n%a==0){
-            cout<<a<<'\n';
-            n/=a;
+    for(int i=2; i*i<=n;i++){
+        while(n%i==0){
+            cout<<i<<'\n';
+            n/=i;
         }
-        else a++;
     }
+    if(n!=1) cout<<n; //1100== 2 * 2 * 5 * 5 * 11 에서 11을 출력하는 코드가 이것.
 }

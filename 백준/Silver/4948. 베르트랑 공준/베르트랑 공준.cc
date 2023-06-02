b1b5581e0c4305fdc876d454e7e439d843f3750d
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-02 21:30:20 +0900

    [Silver II] Title: 베르트랑 공준, Time: 16 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -1,31 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> sieve(int n){
-    vector<int> primes;
-    vector<bool> state(n+1,true);
-    state[1]=false;
-    for(int i=2; i*i<=n;i++){
-        if(!state[i]) continue;
-        for(int j=i*i;j<=n;j+=i)
-            state[j]=false;
-    }
-    for(int i=2; i<=n; i++){
-        if(state[i]) primes.push_back(i);
-    }
-    return primes;
-}
+const int m=123456*2;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    while(true){
-        int n;
+    vector<bool> state(m+1,true);
+    state[1]=false;
+    for(int i=2; i*i<=m; i++){
+        if(!state[i]) continue;
+        for(int j=i*i;j<=m;j+=i)
+          state[j]=false;
+    }
+    int n, cnt;
+    while(1){
         cin>>n;
-        if(n==0) break;
-        vector<int> primes2=sieve(2*n);
-        vector<int> prime1=sieve(n);
-        cout<<primes2.size()-prime1.size()<<'\n';
+        if(n==0)  break;
+        cnt=0;
+        for(int i=n+1;i<=2*n; i++)
+          if(state[i]) cnt++;
+        cout<<cnt<<'\n';
     }
 }

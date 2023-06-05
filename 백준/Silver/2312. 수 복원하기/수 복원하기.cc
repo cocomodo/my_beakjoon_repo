7c50a6ee13ccaeb7db23c4f64dc2e58009638e39
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-05 19:20:04 +0900

    [Silver III] Title: 수 복원하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.cc
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.cc
@@ -1,42 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> state(100'001,true);
-vector<int> sieve(int n){
-    vector<int> primes;
-    state[1]=false;
-    for(int i=2; i*i<=n;i++){
-        if(!state[i]) continue;
-        for(int j=i*i;j<=n; j+=i)
-          state[j]=false;
-    }
-    for(int i=2;i<=n;i++)
-      if(state[i]) primes.push_back(i);
-    
-    return primes;
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t;
+
+    int t; 
     cin>>t;
-    vector<int> primes=sieve(100'001);
     while(t--){
-        int n;
+        int n; 
         cin>>n;
-        vector<pair<int,int>> divider;
-        for(auto p : primes){
+        for(int i=2; i*i<=n;i++){
             int cnt=0;
-            while(n%p==0){
+            while(n%i==0){
                 cnt++;
-                n/=p;
+                n/=i;
             }
-            if(cnt!=0)divider.push_back({p,cnt});            
-            if(n==1) break;
+            if(cnt) cout<<i<<' '<<cnt<<'\n';
         }
-        for(auto q : divider){
-            cout<<q.first<<' '<<q.second<<'\n';
-        }
+        if(n!=1) cout<<n<<' '<<1<<'\n';
     }
 }

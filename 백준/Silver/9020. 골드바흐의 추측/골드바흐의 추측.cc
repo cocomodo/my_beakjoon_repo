1731ada6f76cd04a80235500466af0262929cddf
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-05 19:36:30 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isprime(int n) {
-	if (n == 1) return 0;
-	for (int i = 2; i * i <= n; i++) {
-		if (n % i == 0) 
-			return 0;
-	}
-	
-	return 1;
+vector<bool> state(10'001,true);
+void sieve(int n){
+    vector<int> primes;
+    state[1]=false;
+    for(int i=2;i*i<=n;i++){
+        if(!state[i]) continue;
+        for(int j=i*i;j<=n;j+=i)
+          state[j]=false;
+    }
+    // for(int i=2;i<=n;i++)
+    //   if(state[i]) primes.push_back(i);
+    // return primes;
 }
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int n;
-	cin>>n;
-
-	while (n--) {
-		int a;
-		cin >> a;
-		for (int i = a / 2; i > 1; i--) {
-			if (isprime(i) && isprime(a - i)) {
-				cout << i << " " << a - i<<"\n";
-				break;
-			}
-		}
-	}
+    int t;
+    cin>>t;
+    sieve(10'001);
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=n/2;i>=1;i--)
+            if(state[i]&&state[n-i]) {
+                cout<< i<<' '<<n-i<<'\n';
+                break;
+            }
+    }
 }

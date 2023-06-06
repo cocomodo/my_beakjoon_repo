48f261607c9e2fb571698dece2b19a65fc17f8dd
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-07 03:19:16 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -4,18 +4,17 @@ using namespace std;
 int a[100'005];
 int n;
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	sort(a, a + n);
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		int t;
-		cin >> t;
-		cout << binary_search(a,a+n,t) << "\n";
-	}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0; i<n; i++) cin>>a[i];
+    sort(a,a+n);
+    int m;
+    cin>>m;
+    while(m--){
+        int t;
+        cin>>t;
+        cout<<binary_search(a,a+n,t)<<'\n';
+    }
 }

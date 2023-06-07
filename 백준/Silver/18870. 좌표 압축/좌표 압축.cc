2b005bbb2e8add22e7a9e8eed429133faad25280
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-07 16:30:39 +0900

    [Silver II] Title: 좌표 압축, Time: 468 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -2,25 +2,18 @@
 using namespace std;
 
 int n;
-int a[1'000'005];
-vector<int> tmp, uni;//unique
+int x[1'000'005];
+vector<int> uni;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n;
     for(int i=0; i<n; i++){
-        cin>>a[i];
-        tmp.push_back(a[i]);
+        cin>>x[i];
+        uni.push_back(x[i]);
     }
-    sort(tmp.begin(),tmp.end());
-    for(int i=0; i<n; i++){
-        if(i==0||tmp[i-1]!=tmp[i])
-            uni.push_back(tmp[i]);
-    }
-    for(int i=0; i<n; i++){
-        cout<<lower_bound(uni.begin(),uni.end(),a[i])-uni.begin()<<' ';
-        //lower_bound는 배열이면 포인터를, vector면 iterator를 반환하기 때문에 이런식으로 
-        //처리를 해줘서 index를 반환시킨다.  
-    }
-
+    sort(uni.begin(),uni.end());
+    uni.erase(unique(uni.begin(),uni.end()),uni.end());
+    for(int i=0; i<n; i++)
+        cout<<lower_bound(uni.begin(),uni.end(),x[i])-uni.begin()<<' ';
 }

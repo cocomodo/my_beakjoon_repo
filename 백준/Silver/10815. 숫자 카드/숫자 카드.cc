c324270cc00d2b731a98b3e0ca0349aa05166ad6
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-09 02:16:00 +0900

    [Silver V] Title: 숫자 카드, Time: 220 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int a[500'005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0; i<n; i++) cin>>a[i];
+    sort(a,a+n);
+    cin>>m;
+    while(m--){
+        int t;
+        cin>>t;
+        cout<<binary_search(a,a+n,t)<<' '; 
+    }   
+}

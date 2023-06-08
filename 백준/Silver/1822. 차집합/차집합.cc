59e43d8cede2942a40bb3b66c3e0893101105883
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-09 02:33:44 +0900

    [Silver IV] Title: 차집합, Time: 224 ms, Memory: 9132 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.cc
+++ 백준/Silver/1822. 차집합/차집합.cc
@@ -1,10 +1,3 @@
-// (2 5 11 ((7)) 9 4)
-// 이런 경우 출력 
-// 3
-// 2 5 11
-
-//b에서 a의 원소들을 이분탐색해서 0이 나오면 출력, 1이 나오면 안출력
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,16 +15,11 @@ int main(){
     for(int i=0; i<m; i++) cin>>b[i];
     sort(a,a+n);
     sort(b,b+m);
-    int cnt=0;
     for(int i=0; i<n; i++){
         if(!binary_search(b,b+m,a[i])){
-            cnt++;
             nums.push_back(a[i]);
         }
     }
-    if(cnt!=0){
-        cout<<cnt<<'\n';
-        for(auto c: nums) cout<<c<<' ';
-    }else cout<<cnt;
-
+    cout<<nums.size()<<'\n';
+    for(auto c: nums) cout<<c<<' ';
 }

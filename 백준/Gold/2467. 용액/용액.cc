7e4749c433b7fd3872cf50b4d1b60a3402760bab
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-10 15:09:17 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -9,23 +9,21 @@ int main(){
     cin.tie(0);
     cin>>n;
     for(int i=0; i<n; i++) cin>>a[i];
-    int ans1=1e9+1;
-    int ans2=1e9+1;
-    for(int i=0; i<n; i++){
-        int idx=lower_bound(a,a+n,-a[i])-a;
-        if(idx+1<n && i!=idx+1 && abs(a[i]+a[idx+1])<abs(ans1+ans2)){
-            ans1=a[i];
-            ans2=a[idx+1];
+    int ans=2*1e9+1;
+
+    int st=0;
+    int en=n-1;
+    int ans1, ans2;
+    while(st<en){
+        int cur=a[st]+a[en];
+        int num=abs(cur);
+        if(num<ans){//num= abs(cur)로 정의된 수이기 때문에, 최소값은 0이다. 
+            ans=num;
+            ans1=a[st];
+            ans2=a[en];
         }
-        if(idx<n && i!=idx && abs(a[i]+a[idx])<abs(ans1+ans2)){
-            ans1=a[i];
-            ans2=a[idx];
-        }
-        if(idx!=0 && i!=idx-1 && abs(a[i]+a[idx-1])<abs(ans1+ans2)){
-            ans1=a[i];
-            ans2=a[idx-1];
-        }
+        if(cur>0) en--;
+        else st++;
     }
-    if(ans1>ans2) swap(ans1,ans2);
     cout<<ans1<<' '<<ans2;
 }

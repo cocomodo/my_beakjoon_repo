7a64b81242f81729305177447825a69b5a429c87
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-17 12:21:00 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -2,26 +2,27 @@
 using namespace std;
 
 int n, m;
-// int a[10'005];
-vector<int> v;
+int a[10'005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>m;
-    for(int i=0; i<n; i++){
-        int t;
-        cin>>t;
-        v.push_back(t);
-    }
-    v.push_back(0);
-    int ans=0,st=0,en=1,tot=v[0];
+    for(int i=0; i<n; i++) cin>>a[i];
+    int ans=0,st=0,en=1,tot=a[0];
     //a[n]==0; 전역변수로 선언했기 때문에 초기화가 0으로 되어있어서 불필요한 코드.
     while(1){
         if(tot==m) ans++;
-        if(tot<=m) tot+=v[en++];
-        if(tot>m) tot-=v[st++];
-        if(en==int(v.size())) break;
+        if(tot<=m) tot+=a[en++];
+        if(tot>m) tot-=a[st++];
+        if(en==n+1) break;/*vector코드 형태와 다른부분은 en이 의미하는 바가 
+        vector풀이는 갯수, array 풀이는 인덱스 라는 상황이라는 것이라고 생각된다. 
+        이 경우 array 풀이에서 올바른 경우를 출력하려면, en=0부터 시작을 하거나
+        혹은 en=1로 시작을 한뒤에 맨 마지막 while문 탈출 부분을 vector처럼 갯수
+        형태로 판단하는게 아니라 인덱스값으로 고려해서 판단해주어야 올바른 코드가 된다고
+        보여진다. */
+        //그래서 en=0으로 시작해서 en==n으로 판단하거나, 
+        //en=1로 시작해서 en==n+1로 해야 옳바른 arrary fmf 
     }
     cout<<ans;
 }

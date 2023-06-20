6ebf4298f996c5609dcc11e72b77b34e5483189e
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-20 22:54:54 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 116 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     int n, m;
+    string s, p;
+    unordered_map<string,string> umap;
+
     cin>>n>>m;
-    unordered_map<string,string> site;
-    for(int i=0; i<n; i++){
-        string name,pass;
-        cin>>name>>pass;
-        site[name]=pass;
+    while(n--){
+        cin>>s>>p;
+        umap[s]=p;
     }
     while(m--){
-        string query;
-        cin>>query;
-        cout<<site[query]<<'\n';
+        cin>>s;
+        cout<<umap[s]<<'\n';
     }
 }

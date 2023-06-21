82825191e2816cdd1f371ba75edcdad5aad9515d
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-22 01:12:36 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 544 ms, Memory: 209732 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -4,17 +4,13 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     string s;
-    unordered_set<string> uset;
-    // vector<string> test;
     cin>>s;
-    int en=(int)s.size();
-    for(int i=0; i<en;i++){
-        for(int j=1; j<=en;j++){
-            // test.push_back(s.substr(i,j));
-            uset.insert(s.substr(i,j));
+    unordered_set<string> uset;
+    for(int st=0; st<s.length();st++){
+        for(int len=1; st+len<=s.length();len++){
+            uset.insert(s.substr(st,len));
         }
     }
-    cout<<uset.size()<<'\n';
+    cout<<uset.size();
 }

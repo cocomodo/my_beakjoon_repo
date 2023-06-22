a7e4f65e6060cdd362dc3df17126117662c59a7e
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-22 18:19:24 +0900

    [Silver II] Title: 싸이버개강총회, Time: 68 ms, Memory: 12812 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
@@ -2,44 +2,39 @@
 using namespace std;
 
 int ttoi(string time){
-    int t=0;
+    int t;
     string h, m;
     h=time.substr(0,2);
     m=time.substr(3,2);
-    
-    t=(60*stoi(h))+stoi(m);
+    t=60*(stoi(h))+stoi(m);    
     return t;
 }
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int cnt=0;
 
-    string S,E,Q;
-    cin>>S>>E>>Q;
-
-    int st_t, end_t, stream_t;
-    st_t=ttoi(S);
-    end_t=ttoi(E);
-    stream_t=ttoi(Q);
-
-    unordered_set<string> st_list;
-    unordered_set<string> atd_list;
-
+    unordered_set<string> slist;
+    unordered_set<string> flist;
+    string st_t, en_t, quit_t;
+    cin>>st_t>>en_t>>quit_t;
+    int st_tnum,en_tnum, quit_tnum;
+    st_tnum=ttoi(st_t);
+    en_tnum=ttoi(en_t);
+    quit_tnum=ttoi(quit_t);
     while(true){
-        string c_time,name;
-        cin>>c_time>>name;
+        string ch_t, user_id;
+        cin>>ch_t>>user_id;
         if(cin.eof()==true) break;
-        if(ttoi(c_time)<=st_t){
-            st_list.insert(name);
-        }
-        else if(ttoi(c_time)>=end_t && ttoi(c_time)<=stream_t){
-            if(st_list.find(name)!=st_list.end()){/*위와 같은 조건을 만족하는데,
-            그 조건을 만족하는 것이 st_list에 있다면, atd_list에 담는다.*/
-                atd_list.insert(name);
+        int ch_tnum;
+        ch_tnum=ttoi(ch_t);
+        if(ch_tnum<=st_tnum)
+            slist.insert(user_id);
+        else if(ch_tnum>=en_tnum && ch_tnum<=quit_tnum){
+            if(slist.find(user_id)!=slist.end()){
+                flist.insert(user_id);
             }
         }
     }
-    cout<<atd_list.size()<<'\n';
+    cout<<flist.size();
 }

30fe11f1eeaf718d7404dc3724850d47f41dd803
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-26 03:17:56 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 56 ms, Memory: 7176 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -1,52 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m , p, l , x;
-
-int problevel[300'005];
+int n, m, p, l , x;
+int probToLevel[100'005];
 set<int> probBylevel[105];
+
+
+
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0); 
+    cin.tie(0);
     cin>>n;
     while(n--){
-        cin>>p>>l;
-        problevel[p]=l;
-        probBylevel[l].insert(p);
+        int prob, level;
+        cin>>prob>>level;
+        probToLevel[prob]=level;
+        probBylevel[level].insert(prob);
     }
     cin>>m;
     while(m--){
         string query;
         cin>>query;
-        if(query=="recommend"){
+        if(query=="add"){
+            int prob, level;
+            cin>>prob>>level;
+            probToLevel[prob]=level;
+            probBylevel[level].insert(prob);
+        }
+        else if(query=="recommend"){
             cin>>x;
             if(x==1){
-                for(int i=100;i>=1;i--){
+                for(int i=100; i>=1;i--){
                     if(probBylevel[i].empty()) continue;
-                    auto it1=prev(probBylevel[i].end());
-                    cout<<*it1<<'\n';
+                    auto it=prev(probBylevel[i].end());
+                    cout<<*it<<'\n';
                     break;
                 }
             }
-            else{//x==-1
-                for(int i=1; i<=100;i++){
+            else{//x==-1;
+                for(int i=1; i<=100; i++){
                     if(probBylevel[i].empty()) continue;
-                    auto it2=probBylevel[i].begin();
-                    cout<<*it2<<'\n';
+                    auto it=probBylevel[i].begin();
+                    cout<<*it<<'\n';
                     break;
                 }
             }
-        }
-        else if(query=="add"){
-            int p, q;
-            cin>>p>>q;
-            problevel[p]=q;
-            probBylevel[q].insert(p);
         }
         else if(query=="solved"){
-            int p;
-            cin>>p;
-            probBylevel[problevel[p]].erase(p);
+            int q;
+            cin>>q;
+            probBylevel[probToLevel[q]].erase(q);
         }
     }
 }

62645ece9fb5bdf7f4ca9b42a5bdbcd3defe9c80
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-26 20:35:59 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n; 
+priority_queue<int> pq;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    while(n--){
+        int t;
+        cin>>t;
+        if(t==0){
+            if(pq.empty()) cout<<0<<'\n';
+            else{
+                cout<<pq.top()<<'\n';
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(t);
+        }
+    }
+}

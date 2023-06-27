706678265a4f7461118b13ebec50b79b1f2d5840
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-27 22:09:25 +0900

    [Silver II] Title: N번째 큰 수, Time: 400 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,19 +1,19 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-
-int a[2'250'000];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n;
+
+    int n, num;
+    priority_queue<int, vector<int> , greater<>> pq;
+
     cin>>n;
-    int t;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>> a[i*n+j];
-        }
+    for(int i=0; i<n*n; i++){
+        cin>>num;
+        pq.push(num);
+        if(n<pq.size()) pq.pop();//원래 n<(int)pq.size() 형태로 작성하는것이 정석이지만 이렇게 제출해봄
     }
-    sort(a,a+n*n);
-    cout<<a[n*n-1-(n-1)];
+    cout<<pq.top();
 }

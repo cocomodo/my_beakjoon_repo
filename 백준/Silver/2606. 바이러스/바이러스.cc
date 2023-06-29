3f249eb1f7c39ba49424cc3d4abfecdd7ad8a41b
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-06-29 23:24:17 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -4,36 +4,37 @@ using namespace std;
 vector<int> adj[105];
 bool vis[105];
 
-int n, m;
+
+int n, m; 
+int ans;
+
+void dfs(int cur){
+    vis[cur]=true;
+    for(auto nxt: adj[cur]){
+        if(vis[nxt]) continue;
+        ans++;
+        dfs(nxt);
+    }
+
+}
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    cin>>n>>m;
 
-    //그래프 입력
-    for(int i=0; i<m; i++){
+    cin>>n>>m;
+    while(m--){
         int u, v;
-        cin>>u>>v;
+        cin>>u>> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    //BFS
-    
-    queue<int> q; 
-    q.push(1);
-    vis[1]=true;
-    int ans=0;
-    while(!q.empty()){
-        int cur=q.front(); q.pop();
-        for(auto nxt : adj[cur]){
-            if(vis[nxt]) continue;
-            q.push(nxt);
-            vis[nxt]=true;
-            ans++;
-        }
-    }
+    dfs(1);
     cout<<ans;
+
 }
+/*
+방문 순서가 중요한것이 아니라 방문을 몇개의 컴퓨터에 했는지가 중요하므로, 
+dfs를 활용한 풀이로 코드 작성을 간결하게 만들 수 있다. 
+*/

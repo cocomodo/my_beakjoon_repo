7769502aede802962cf28a8d3a24ed5f24a074cb
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-05 14:09:00 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,38 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n,q;
 vector<pair<int,int>> adj[1'005];
-int n, m, u, v, d;
+int vis[1'005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    cin>>n>>m;
-
+    cin>>n>>q;
     for(int i=0; i<n-1; i++){
+        int u, v, d;
         cin>>u>>v>>d;
         adj[u].push_back({v,d});
         adj[v].push_back({u,d});
     }
 
-    while(m--){
-        queue<pair<int,int>> q;
-        vector<bool> vis(n+1);
+    while(q--){
+        fill(vis,vis+n+1,0);
+        int u, v;
         cin>>u>>v;
+
+        queue<pair<int,int>> q;
         q.push({u,0});
         vis[u]=true;
-        
         while(!q.empty()){
-            auto [cur,dist] = q.front(); q.pop();
+            auto [cur,dist] =q.front(); q.pop();
             if(cur==v){
                 cout<<dist<<'\n';
-                break;
+                break; //break면 while문을 깨고 나갈것이고, continue면 아래 내용을 건너뛰고 다음 반복단계로 들어갈것이다.
             }
-            for(auto [nxt,nxtDist]: adj[cur]){
+            for(auto [nxt,nxtDist]:adj[cur]){
                 if(vis[nxt]) continue;
                 vis[nxt]=true;
-                q.push({nxt,dist+nxtDist});
+                q.push({nxt,nxtDist+dist});
             }
         }
 

a80d746fcafdc39c014adc95e6080e0534b5de68
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-05 13:50:45 +0900

    [Gold IV] Title: 트리 순회, Time: 32 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.cc
+++ 백준/Gold/22856. 트리 순회/트리 순회.cc
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, c, l, r, root=1;
+const int root=1;
+int n, c, l, r;
 int lc[100'005];
 int rc[100'005];
 int p[100'005];
-vector<bool> vis(100'005);
+int vis[100'005];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
     cin>>n;
-    // p[root]=-1;
-    for(int i=0; i<n; i++){
+    // p[root]=-1; /* 이 코드 중요. 분석해볼곳*/
+    for(int i=1; i<=n; i++){
         cin>>c>>l>>r;
-
-        /* 여기서 -1 이어도 이런 식으로 받는게 중요하다.*/
-            lc[c]=l;
-            p[l]=c;
-        
-        
-            rc[c]=r;
-            p[r]=c;
-       
+        lc[c]=l;
+        rc[c]=r;
+        p[l]=c;
+        p[r]=c;
     }
-
+    
     int endpoint=root;
     while(rc[endpoint]!=-1){
         endpoint=rc[endpoint];
     }
+    
     int cur=root;
-    int cnt=0;
+    int move=0;
     while(true){
-        vis[cur]=true;
-        cnt++;
-        if(lc[cur]!=-1 && !vis[lc[cur]])
-            cur=lc[cur];
-        else if(rc[cur]!=-1 && !vis[rc[cur]])
-            cur=rc[cur];
-        else{
-            if(endpoint==cur)
-                break;
-            else
-                cur=p[cur];
+        vis[cur]=true; /* 이 코드 중요. 분석해볼곳*/
+        move++;
+        // cout<<cur<<' ';
+        if(lc[cur]!=-1 && !vis[lc[cur]]) cur=lc[cur];
+        else if(rc[cur]!=-1 && !vis[rc[cur]]) cur=rc[cur];
+        else {
+            if(cur==endpoint) break;
+            else cur=p[cur];
         }
     }
-    cout<<cnt-1;
+    cout<<move-1;
+
 }

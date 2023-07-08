4bfc64e2d79a96a3b1ffc0c6336e3ae4238bb0a9
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-08 23:10:28 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -2,46 +2,38 @@
 using namespace std;
 
 int n, m;
-int ideg[1005];
+int id[1005];
 vector<int> adj[1005];
-vector<int> sq;
+vector<int> result;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin>>n>>m;
-    
-    int q,u,v;
-    for(int i=0; i<m; i++){
-        cin>>q;
-        if(q==0) continue;
+
+    int no,u,v;
+    while(m--){
+        cin>>no;
+        if(no==0) continue;
         cin>>u;
-        while(--q){
+        while(--no){
             cin>>v;
             adj[u].push_back(v);
-            ideg[v]++;
-            swap(u,v); /*스왑을 사용할 생각을 하지못했구나. 스왑이라는걸 
-            통해서 u는 한번만 입력받아서 남기고, 그 뒤에 v를 입력받으면서 스왑으로
-            v를 u로 넘기면 된다*/
+            id[v]++;
+            u=v;//swap(u,v); 를 쓰는게 깔끔해 보이긴 하는데, 코드 동작상 이렇게 해도 된다.
         }
     }
-    queue<int> que;
-    vector<int> outval;
-    for(int i=1; i<=n; i++){
-        if(ideg[i]==0) que.push(i);
+    queue<int> q;
+    for(int i=1; i<=n; i++)
+        if(id[i]==0) q.push(i);
+    
+    while(!q.empty()){
+        int cur=q.front(); q.pop();
+        result.push_back(cur);
+        for(int nxt: adj[cur])
+            if(--id[nxt]==0) q.push(nxt);
     }
-    while(!que.empty()){
-        int cur=que.front(); que.pop();
-        outval.push_back(cur);
-        for(int nxt: adj[cur]){
-            ideg[nxt]--;
-            if(ideg[nxt]==0) que.push(nxt);
-        }
-    }
-    if(outval.size()!=n) cout<<0;
-    else {
-        for(auto c: outval) cout<<c<<'\n';
-    }
-
+    if(result.size()!=n) cout<<0;
+    else for(auto s: result) cout<<s<<'\n';
 }

954277cd7c370e818ec60f88fcc8e0ff7ab58c02
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-14 00:06:19 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int d1,d2;
+    cin>>d1>>d2;
+    double c=3.141592;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<2*d2*c+2*d1;
+}

bb5eebe2e1828e2424ce287710cb46919db0868d
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-14 09:04:01 +0900

    [Gold IV] Title: 행성 연결, Time: 156 ms, Memory: 19416 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.cc
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -1,49 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int p[1002];
-int find(int x) {
-  if(p[x] < 0) return x;
-  return p[x] = find(p[x]);
-}
+vector<pair<int,int>> adj[1'005];
+bool chk[1'005];
 
-bool merge(int u, int v) {
-  u = find(u), v = find(v);
-  if(u == v) return 0;
-  if(p[u] == p[v]) p[u]--;
-  if(p[u] < p[v]) p[v] = u;
-  else p[u] = v;
-  return 1;
-}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-vector< tuple<int, int, int> > e;
+    int n;
+    cin>>n;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int n; cin >> n;
-  fill(p, p + n, -1);
-
-  int c;
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < n; j++) {
-      cin >> c;
-      if(i == j) continue;
-      e.push_back({c, i, j});
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            int cost;
+            cin>>cost;
+            if(i==j) continue;
+            adj[i].push_back({cost,j});
+        }
     }
+    
+    priority_queue< tuple<int,int,int>,
+                    vector<tuple<int,int,int>>,
+                    greater<tuple<int,int,int>>> pq;
+    
+    chk[1]=true;
+    for(auto nxt: adj[1]){
+        if(chk[nxt.second]!=true) pq.push({nxt.first,1,nxt.second});
+    }
+    long long ans=0;
+    int cnt=0;
+    while(cnt<n-1){
+        int cost, a, b;
+        tie(cost, a, b)=pq.top(); pq.pop();
+        if(chk[b]) continue; //이걸 빼먹었네. 아주 중요한데 이걸 빼먹었어. 
+        chk[b]=true;
+        ans+=cost;
+        cnt++;
+        for(auto nxt: adj[b]){
+            if(chk[nxt.second]) continue;
+            pq.push({nxt.first,b,nxt.second});
 
-  sort(e.begin(), e.end());
-  
-  int u, v;
-  int cnt = 0;
-  long long ans = 0;
-  for(auto edge : e) {
-    tie(c, u, v) = edge;
-    if(!merge(u, v)) continue;
-    ans += c;
-    cnt++;
-    if(cnt == n - 1) break;
-  }
-  cout << ans;
+        }
+    }
+    cout<<ans;
+
 }

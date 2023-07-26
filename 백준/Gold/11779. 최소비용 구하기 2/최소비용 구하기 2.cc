4f3a8f44344261900258bf7c591233ad2ba662dc
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-07-26 19:14:59 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, st, en;
+int n , m , st, en;
 vector<pair<int,int>> adj[1'005];
 int d[1'005];
-const int INF=0x3f3f3f3f;
+const int INF= 0x3f3f3f3f;
 int pre[1'005];
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin>>n>>m;
     while(m--){
         int u, v, w;
         cin>>u>>v>>w;
         adj[u].push_back({w,v});
     }
+    fill(d,d+1+n,INF);
     cin>>st>>en;
-
-    fill(d,d+n+1,INF);
-
     priority_queue< pair<int,int>,
                     vector<pair<int,int>>,
                     greater<pair<int,int>>> pq;
-
+                
     d[st]=0;
     pq.push({d[st],st});
-
     while(!pq.empty()){
-        auto cur=pq.top(); pq.pop();
+        auto cur = pq.top(); pq.pop();
         if(d[cur.second]!=cur.first) continue;
         for(auto nxt: adj[cur.second]){
-            if(d[cur.second]+nxt.first>=d[nxt.second]) continue;
+            if(d[nxt.second]<=d[cur.second]+nxt.first) continue;
             d[nxt.second]=d[cur.second]+nxt.first;
             pq.push({d[nxt.second],nxt.second});
-            pre[nxt.second]=cur.second;
+            pre[nxt.second] = cur.second;
         }
     }
     cout<<d[en]<<'\n';
+    
     vector<int> path;
-    int cur=en;
+    int cur= en;
     while(cur!=st){
         path.push_back(cur);
-        cur=pre[cur];
+        cur= pre[cur];
     }
-    path.push_back(cur);//st 가 된 cur 값을 담아주기. 위에서 안담기기 때문에. 
+    path.push_back(cur);
     reverse(path.begin(),path.end());
     cout<<path.size()<<'\n';
-    for(auto x: path) cout<<x<<' ';
+    for(auto c: path){
+        cout<<c<<' ';
+    }
 }

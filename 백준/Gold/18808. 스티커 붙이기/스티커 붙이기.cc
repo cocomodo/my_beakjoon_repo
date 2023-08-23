9ce4c7bdf144af41a6e5d189478f005c0f010a40
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-08-24 00:41:03 +0900

    [Gold III] Title: 스티커 붙이기, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -8,14 +8,14 @@ int paper[12][12];
 
 void rotate(){
     int tmp[12][12];
-    
+
     for(int i=0; i<r; i++)
         for(int j=0; j<c; j++)
             tmp[i][j]=paper[i][j];
-    
     for(int i=0; i<c; i++)
         for(int j=0; j<r; j++)
             paper[i][j]=tmp[r-1-j][i];
+    
     swap(r,c);
 }
 
@@ -23,7 +23,7 @@ bool pastable(int x, int y){
     for(int i=0; i<r; i++){
         for(int j=0; j<c; j++){
             if(note[x+i][y+j]==1 && paper[i][j]==1)
-                return false; 
+                return false;
         }
     }
     for(int i=0; i<r; i++){
@@ -44,11 +44,12 @@ int main(){
         for(int i=0; i<r; i++)
             for(int j=0; j<c; j++)
                 cin>>paper[i][j];
+        
         for(int rot=0; rot<4; rot++){
-            bool is_paste = false;
+            bool is_paste=false;
             for(int x=0; x<=n-r; x++){
                 if(is_paste) break;
-                for(int y= 0; y<=m-c; y++){
+                for(int y=0; y<=m-c; y++){
                     if(pastable(x,y)){
                         is_paste=true;
                         break;
@@ -62,6 +63,6 @@ int main(){
     int cnt=0;
     for(int i=0; i<n; i++)
         for(int j=0; j<m; j++)
-            cnt+=note[i][j];
-    cout<<cnt<<'\n';
+            cnt+=(note[i][j]==1);
+    cout<<cnt;
 }

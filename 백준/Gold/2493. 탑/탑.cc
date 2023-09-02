84c84860eae71d1d039514cdee68d89e7b8bf801
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-03 01:32:30 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -2,21 +2,20 @@
 using namespace std;
 
 int n;
-stack<pair<int,int>> tower;
+stack<pair<int,int>> s;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     cin>>n;
-    tower.push({100'000'005,0});
+    s.push({100'000'005,0});
     for(int i=1; i<=n; i++){
         int h;
         cin>>h;
-        while(tower.top().first<=h){
-            tower.pop();
-        }
-        cout<<tower.top().second<<' ';
-        tower.push({h,i});
+        while(s.top().first<=h)
+            s.pop();
+        cout<<s.top().second<<' ';
+        s.push({h,i});
     }
 }

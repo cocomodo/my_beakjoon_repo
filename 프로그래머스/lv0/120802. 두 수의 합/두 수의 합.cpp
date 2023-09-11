648223b4f579b4a02dbcb3f2cddcd923c5404321
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-11 21:45:33 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.cpp
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
 
-int solution(int a, int b) {
-    return a+b;
+int solution(int num1, int num2) {
+    int answer;
+    answer = num1 + num2;
+    return answer;
+}
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    int num1, num2;
+    std::cin >> num1 >> num2;
+    std::cout << solution(num1, num2);
 }

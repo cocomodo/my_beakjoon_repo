fed6631a1c77779d7af4766a9402b281ddb0bd5f
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-11 19:47:51 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
-typedef long long ll;
 using namespace std;
+
 string s;
-ll ans = 0;
+long long ans=0;
 stack<char> st;
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> s;
-  int sz = s.length();
-  for (int i = 0; i < sz; i++) {
-    if (s[i]=='(')
-      st.push(s[i]);
-    else {
-      if (s[i-1] == '(') { // 레이저일 경우
-        st.pop(); // 앞에서 막대라고 착각하고 stack에 s[i]를 넣었으므로 pop
-        ans+=st.size(); // 막대의 개수만큼 ans에 추가
-      }
-      else { // 막대의 끝일 경우
-        st.pop();  // 막대의 개수를 1 감소
-        ans++; // 막대 1개가 절단된 것과 동일한 상황이므로 ans에 1 추가
-      }
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>s;
+    int sz=s.length();
+    for(int i=0; i<sz; i++){
+        if(s[i]=='(')
+            st.push(s[i]);
+        else{//')' 인 경우
+            if(s[i-1]=='('){
+                st.pop();
+                ans+=st.size();
+            }
+            else{
+                st.pop();
+                ans++;
+            }
+        }
     }
-  }
-  cout << ans << "\n";
-  return 0;
+    cout<<ans<<'\n';
 }

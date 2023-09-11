cd49bae416de4c5b084e3d7cc081dce0fc1d72a0
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-11 21:39:07 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.cpp
+++ 프로그래머스/lv0/120804. 두 수의 곱/두 수의 곱.cpp
@@ -1,10 +1,8 @@
-// #include <string>
-// #include <vector>
 #include <bits/stdc++.h>
 
-using namespace std;
 int answer;
 int solution(int num1, int num2) {
     answer=num1*num2;
+    std::cout<<"되나?";
     return answer;
 }

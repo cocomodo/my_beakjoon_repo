4aea51805dfee2da6f7b99b43210bfc7b75a8770
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-14 10:08:10 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 int solution(int a) {
-    int answer = 0;
-    if(a>=100'000 && a<300'000)
-        a*=0.95;
-    else if(a>=300'000 && a<500'000)
-        a*=0.9;
-    else if(a>=500'000)
+    if(a>=500'000)
         a*=0.8;
+    else if(a>=300'000)
+        a*=0.9;
+    else if(a>=100'000)
+        a*=0.95;
     return a;
 }

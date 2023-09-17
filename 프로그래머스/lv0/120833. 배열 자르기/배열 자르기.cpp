bbbe8c8c92be3b04233720b63017794cf978611e
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-17 11:14:26 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.cpp
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 vector<int> solution(vector<int> numbers, int num1, int num2) {
-    vector<int> answer;
-    for(int i=num1; i<=num2;i++){
-        answer.emplace_back(numbers[i]);
-    }
+    vector<int> answer(numbers.begin()+num1,numbers.begin()+num2+1);
     return answer;
 }

31a0c189a0e4270cd1b250bfaf0854e8bbc8a31b
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-21 11:44:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.cpp
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 
 using namespace std;
-
-int d[11];
-
+int factorial(int n)
+{
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
 int solution(int n) {
-    int answer = 0;
-    d[1]=1;
-    for(int i=2; i<=10;i++){
-        d[i]=d[i-1]*i;
-    }
-    for(int i=1; i<=10; i++){
-        if(n>=d[i]) answer=i;
-    }
-    return answer;
+    int i = 0;
+    while(factorial(i) <= n)
+        i++;
+    i--;
+    return i;
 }

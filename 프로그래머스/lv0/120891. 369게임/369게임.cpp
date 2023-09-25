e1c85b856a1940b9cebb45909beff9d942d4741d
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-09-25 13:53:20 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    int num=0;
+    while(order>0){
+        num=order%10;
+        if(num!=0&&num%3==0) answer++;
+        order/=10;
+    }
+    return answer;
+}

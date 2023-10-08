deafa5ad40816e0b31065fdf6acdfd7200fe59b0
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-10-08 22:28:30 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
@@ -1,28 +1,19 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 int solution(vector<vector<int>> lines) {
+    vector<int> arr(201,0);
+
     int answer = 0;
-    int a[210]={};
-    for(auto t:lines){
-        int st=t[0];
-        int en=t[1]; // st<en 이 보장됨.
-        for(int i=st; i<=en;i++){
-            a[i+100]++;
+    for(auto& v : lines){
+        for(int i = v[0]; i < v[1]; ++i){
+            arr[i+100]++;
         }
     }
-    int tmp=-1;
-    for(int j=0; j<210; j++){
-        if(a[j]>=2) tmp++;
-        else {
-            if(tmp>=0) answer+=tmp;
-            tmp=-1;
-        }
+    for(const auto v : arr){
+        answer += v > 1;
     }
-    sort(lines.begin(),lines.end());
-    if((lines[1][1]-lines[1][0]==1)&& (lines[0][1]==lines[1][0]&&lines[1][1]==lines[2][0])) answer--;
     return answer;
 }

4df1916e4c4c8930d9976b8c93ce80f344a725a9
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-10-28 18:50:19 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    priority_queue<int> pq;
-    queue<pair<int,int>> q;
-    for(auto c: priorities) pq.push(c);
-    for(int i=0; i<priorities.size();i++) q.push({priorities[i],i});
-    int t=1;
-    
-    while(!q.empty()){
-        if(q.front().first==pq.top()){
-            if(q.front().second==location)  return t;
-            q.pop();
-            pq.pop();
-            t++;
-        }else{
-            pair<int,int> tmp=q.front();
-            q.push(tmp);
-            q.pop();
-        }
-    }
     return answer;
+    priority_queue< int,
+                    vector<int>,
+                    less<int>> pq;
+    for(auto t: priorities) pq.push(t);
+    cout<<pq.size()<<'\n';
+    for(int i=0; pq.size();i++){
+        cout<<pq.top()<<'\n';
+        pq.pop();
+    }
 }

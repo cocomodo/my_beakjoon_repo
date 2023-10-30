165e2f03727645b6c83dda1c99aa660279f066d8
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-10-31 00:48:05 +0900

    [level 2] Title: 주식가격, Time: 14.40 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,22 +1,28 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-vector<int> solution(vector<int> prices) {
-    vector<int> answer;
-    
-    for(int i=0; i<prices.size()-1;i++){
-        int cnt=0;
-        for(int j=i+1; j<prices.size(); j++){
-            cnt++;
-            if(prices[i]>prices[j]) {
-                answer.push_back(cnt);
-                break;
+vector<int> solution(vector<int> prices) 
+{
+    vector<int> answer(prices.size(),0);
+    vector<int> stack;
+
+    for(int i = 0 ; i < prices.size() - 1; i++){
+        if(stack.empty() || prices[i] >= prices[stack.back()]){
+            stack.push_back(i);
+        }
+        else{
+            while(!stack.empty() && prices[i] < prices[stack.back()]){
+                stack.pop_back();
             }
-            if(cnt==prices.size()-1-i) answer.push_back(cnt);
+            stack.push_back(i);
+        }
+
+        for(auto &x : stack){
+            answer[x]++;
         }
     }
-    answer.push_back(0);
-    
+
     return answer;
 }

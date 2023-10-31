42842f871197d57a9fadd48e3f42f5d52e0f5272
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-10-31 17:14:56 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -2,15 +2,13 @@
 
 using namespace std;
 
-int solution(vector<int> v) {
-    int answer = 0;
-    for(int i=v.size();i>=0;i--){
-        int cnt=0;
-        for(auto t: v) {
-            if(t>=i) cnt++; 
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] <= i) {
+            return i;
         }
-        if(cnt>=i) answer=max(answer,i);
     }
-    return answer;
+    return citations.size();
 }
-//6편의 논문은, 5편의 논문은, 4편의 논문은 등등등.. 테스트 해보면서 내리자. 

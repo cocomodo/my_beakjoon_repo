2fb87f9078128c7c1de3a659f20cacea2f95e236
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-11-08 13:38:19 +0900

    [level 2] Title: 소수 찾기, Time: 0.06 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -2,36 +2,36 @@
 
 using namespace std;
 
-map<int, bool> M;
+map<int,bool> M;
 
 bool isPrime(string num){
-    int n = stoi(num);
-    
-    if(M[n]) return false; 
-    M[n] = true;
-    if(n == 1 || n == 0) return false;
-    if(n == 2) return true;
-    
-    for(int i=2; i<=sqrt(n); i++){
-        if(n % i == 0){
-            M[n] = false;
+    int n=stoi(num);
+
+    if(M[n]) return false;
+    M[n] =true;
+    if(n==1||n==0) return false;
+    if(n==2) return true;
+
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+            M[n]= false;
             return false;
-        }              
+        }
     }
     M[n] = true;
     return true;
 }
 
-int solution(string numbers) {
-    int answer = 0;
+int solution(string numbers){
+    int answer=0;
 
     sort(numbers.begin(), numbers.end());
     do{
         for(int i=1; i<=numbers.size(); i++){
-            string num = numbers.substr(0, i);         
+            string num=numbers.substr(0,i);
             if(isPrime(num)) answer++;
         }
-    }while(next_permutation(numbers.begin(), numbers.end())); //next_permutation을 통해서 다음 순열로 테스트
-    
+    }while(next_permutation(numbers.begin(),numbers.end()));
+
     return answer;
 }

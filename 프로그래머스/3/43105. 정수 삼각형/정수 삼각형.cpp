9c969ba427e6907ea391e821477124f014ed69e8
cocomodo <128673078+cocomodo@users.noreply.github.com>
2023-11-17 20:26:12 +0900

    [level 3] Title: 정수 삼각형, Time: 0.31 ms, Memory: 7.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -2,15 +2,14 @@
 
 using namespace std;
 
-int d[505][505];
-
-int solution(vector<vector<int>> triangle) {
+int solution(vector<vector<int>> t) {
     int answer = 0;
-    int n=triangle.size();
-    d[1][1]=triangle[0][0];
-    for(int i=2; i<=n; i++)
-        for(int j=1; j<=i; j++)
-            d[i][j]=max(d[i-1][j-1],d[i-1][j]) +triangle[i-1][j-1];//d[i-1][0] 의 경우 0
-    answer=*max_element(d[n]+1,d[n]+n+1);
+
+    for (int i = t.size() - 1; i > 0 ; i--){
+        for (int j = 0; j < t[i].size() - 1; j++){
+            (t[i][j] > t[i][j + 1]) ? t[i - 1][j] += t[i][j] : t[i-1][j]+=t[i][j+1];
+        }
+    }
+    answer = t[0][0];
     return answer;
 }
